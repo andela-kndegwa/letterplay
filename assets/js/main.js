d2623a49077759f5aeb9f0c626f89245cf045c49
Kimani Ndegwa <kimani.ndegwa@andela.com>
2017-03-17 11:45:28 +0300

    (fx) Fixes bug with the two loops within the gamePlay function. Break works well

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,8 @@
     var wordChallenge = document.getElementById('wordChallenge');
     var wordResponse = document.getElementById('wordResponse');
     var mySolutions = document.getElementById('solutions')
-    // prevent copy and paste of information into the input box
-    //use case 
+        // prevent copy and paste of information into the input box
+        //use case 
     wordResponse.onpaste = function(e) {
         e.preventDefault();
     }
@@ -14,50 +14,42 @@
 
     function resetHighlights(el) {
         for (var ind = 0; ind < el.children.length; ind++) {
-            if (el.children[ind].classList.contains('text-highlight')) {
-                el.children[ind].classList.remove('text-highlight')
-            }
+            el.children[ind].classList.remove('text-highlight')
         }
     }
+
     var strippedWordChallenge = wordChallenge.innerText.replace(/\s+/g, ""); // Explicit spaces
 
     function gamePlay(responseValue) {
-        var typedResponseLength = wordResponse.value.length
+        var typedResponse = wordResponse.value
+        var typedResponseLength = typedResponse.length
         for (var l = 0; l < typedResponseLength; l++) {
-            // scan through all the letters
-            // debugger;
-            var letterIndex = strippedWordChallenge.indexOf(wordResponse.value[l]);
+            var letterIndex = strippedWordChallenge.indexOf(typedResponse[l]);
             if (letterIndex > -1) {
                 var challengeLength = strippedWordChallenge.length
-                var typedResponse = wordResponse.value
                 for (var b = 0; b < challengeLength; b++) {
-                    var eachLetterInChallenge = wordChallenge.children[l].innerHTML
-                    var eachLetterInResponse = typedResponse[b]
-                    if (eachLetterInResponse == eachLetterInChallenge) {
-                        if (!wordChallenge.children[b].classList.contains("text-highlight")) {
-                            return wordChallenge.children[b].classList.add('text-highlight');
-                         
+                    var eachLetterInChallenge = wordChallenge.children[b].innerHTML
+                    var eachLetterInResponse = typedResponse[l]
+                    if (eachLetterInResponse === eachLetterInChallenge) {
+                        if (!wordChallenge.children[b].classList.contains('text-highlight')) {
+                            wordChallenge.children[b].classList.add('text-highlight');
+                            break;
                         }
+
                     }
 
                 }
-
             }
         }
 
     }
 
-
-    wordResponse.addEventListener('keyup',function(e){
+    // reset the highlights regardless of the key
+    wordResponse.addEventListener('keyup', function(e) {
         e.preventDefault();
-        console.log(e.target.value);
-        if(e.keyCode === 8){
-            resetHighlights(wordChallenge); // resets everything from the highlights
-            console.log(wordChallenge.children);
-            gamePlay(e.target.value);
-        } else{
-            gamePlay(e.target.value);
-        }
+        resetHighlights(wordChallenge)
+        gamePlay(e.target.value);
+
 
     })
 
@@ -77,4 +69,3 @@
             resetHighlights(wordChallenge);
         }
     })
-
