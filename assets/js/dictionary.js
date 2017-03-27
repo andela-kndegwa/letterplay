af757fdd66d2fb8763cb7efa5fdf8d74a77eb227
Kimani Ndegwa <kimani.ndegwa@andela.com>
2017-03-27 09:26:01 +0300

    (fx) Adds monospace font as main font

--- assets/js/dictionary.js
+++ assets/js/dictionary.js
@@ -3,7 +3,7 @@
 var referenceDictionary = {
     'letterplay': ['letter', 'play', 'real', 'pay', 'lay', 'ray', 'let', 'peal', 'pet', 'eat','peat', 'tray', 'pray', 'tea', 'eat', 'tree', 'replay', 'repay', 'treat', 'late', 'pale', 'year'],
     'awesome' : ['awe', 'some', 'ewe', 'see', 'seam', 'seem'],
-    'available' : ['mat', 'able', 'bill', 'bile', 'ail', 'label', 'veil', 'bale', 'avail', 'pail'],
+    'available' : ['mat', 'able', 'bill', 'bile', 'ail', 'label', 'veil', 'bale', 'avail', 'pail', 'ball', 'bell', 'bill', 'viable'],
     'appealing' : ['peal','pan','pile', 'pail', 'ping', 'appeal', 'pig', 'pale', 'pine', 'ape','pea', 'gape', 'apple', 'pane'],
     'undeniable' : ['able','lane', 'able','bale','line', 'bed', 'dine', 'bun', 'bile', 'lean', 'blend', 'be']
 }
